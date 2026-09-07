3b644c196ef549d4f8ad56e5055a60fb9b8f2be8
Cristhian Apaza <cristhianapazamamani48@gmail.com>
2026-09-07 17:03:33 -0400

    Unificar acceso frontend y backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://backend:3001/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
